a019aa54df344f32fd4ecac111ff4896fc3316d2
BaraMac <baarabetswe@gmail.com>
2025-05-19 15:15:09 +0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const ACCESS_PASSWORD = "p208demo"; // change anytime
-export const AUTHORIZED_STAFF = ["77007", "20886", "12345"]; // whitelist
+export const AUTHORIZED_STAFF = ["77007", "20886", "44709", "12345"]; // whitelist
